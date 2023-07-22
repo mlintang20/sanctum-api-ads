06b9ef96a4ae1743543ce337ef47c116826d1485
mlintangp <lintangpanjerino.2002@gmail.com>
2023-07-22 12:01:27 +0700

    add migration

---
+++ database/migrations/2023_07_21_002118_create_cuti_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cuti', function (Blueprint $table) {
+            $table->string('nomor_induk');
+            $table->date('tanggal_cuti');
+            $table->integer('lama_cuti');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cuti');
+    }
+};
